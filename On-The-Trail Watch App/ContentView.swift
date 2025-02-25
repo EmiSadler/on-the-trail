91eef8c356e018c4f09af6864e7bb20a3d367b80
Emily Sadler <emily@sadler.gg>
2025-02-25 14:02:52 +0000

    Initial Commit

---
+++ On-The-Trail Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  On-The-Trail Watch App
+//
+//  Created by Emily Sadler on 25/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ On-The-Trail Watch App/On_The_TrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_The_TrailApp.swift
+//  On-The-Trail Watch App
+//
+//  Created by Emily Sadler on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct On_The_Trail_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
